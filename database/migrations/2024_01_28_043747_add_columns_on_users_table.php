9c8377243e0f6965399c74824503be1f2a8735cc
Ryan Chandra Kusuma <85665855+ryanchandrakusuma@users.noreply.github.com>
2024-01-28 14:19:52 +0700

    Migrations users table and user credit activities table (#2)
    * migrations : create users and credit activities table
    
    add columns to default users table and create user credit activities table
    
    * migrations : added down for create users table

---
+++ database/migrations/2024_01_28_043747_add_columns_on_users_table.php
@@ -0,0 +1,32 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class () extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::table('users', function (Blueprint $table) {
+            $table->string('firstName', 100);
+            $table->string('lastName', 100)->nullable();
+            $table->integer('credits');
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::table('users', function (Blueprint $table) {
+            $table->dropColumn('firstName');
+            $table->dropColumn('lastName');
+            $table->dropColumn('credits');
+        });
+    }
+};
