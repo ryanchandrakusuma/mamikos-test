094fc2c54e647de3d65a4877b80a0ec6c24d0e5c
Ryan Chandra Kusuma <rookie3ryan@gmail.com>
2024-01-28 15:15:55 +0700

    migrations : create user kost requests table
    create user kost requests table for asking room availability service

---
+++ database/migrations/2024_01_28_081322_create_user_kost_requests_table.php
@@ -0,0 +1,33 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('user_kost_requests', function (Blueprint $table) {
+            $table->id();
+            $table->foreignId('user_id')->constrained()->cascadeOnDelete();
+            $table->foreignId('kost_id')->constrained()->cascadeOnDelete();
+            $table->string('question', 255);
+            $table->string('answer', 255)->nullable();
+            $table->boolean('is_resolved')->default(false);
+
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('user_kost_requests');
+    }
+};
