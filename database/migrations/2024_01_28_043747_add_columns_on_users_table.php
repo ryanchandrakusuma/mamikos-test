ff43ed0a4179f7656acac7685066e0e81a6a6c44
Ryan Chandra Kusuma <rookie3ryan@gmail.com>
2024-01-28 14:09:22 +0700

    migrations : added down for create users table

--- database/migrations/2024_01_28_043747_add_columns_on_users_table.php
+++ database/migrations/2024_01_28_043747_add_columns_on_users_table.php
@@ -23,6 +23,10 @@ return new class () extends Migration
      */
     public function down(): void
     {
-        //
+        Schema::table('users', function (Blueprint $table) {
+            $table->dropColumn('firstName');
+            $table->dropColumn('lastName');
+            $table->dropColumn('credits');
+        });
     }
 };
