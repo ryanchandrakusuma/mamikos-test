afdea6cd3a79027a9a0526f56239a2af6f368d3b
Ryan Chandra Kusuma <rookie3ryan@gmail.com>
2024-01-29 20:25:06 +0700

    routes : delete kost
    added routes to delete kost

--- routes/api.php
+++ routes/api.php
@@ -29,5 +29,6 @@ Route::middleware('auth:owners')->group(function () {
     Route::middleware('role:owner')->group(function () {
         Route::post('/kosts', [KostController::class, 'store'])->name('api.kost.store');
         Route::put('/kosts/{id}', [KostController::class, 'update'])->name('api.kost.update');
+        Route::delete('/kosts/{id}', [KostController::class, 'destroy'])->name('api.kost.destroy');
     });
 });
