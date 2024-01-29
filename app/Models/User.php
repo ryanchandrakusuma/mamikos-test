ec1e851b6d0d555f24e6e452c4ac4fa6c03b0ef3
Ryan Chandra Kusuma <rookie3ryan@gmail.com>
2024-01-29 22:43:08 +0700

    model : user
    change package to passport and add hasroles for model traits.

--- app/Models/User.php
+++ app/Models/User.php
@@ -6,13 +6,17 @@ namespace App\Models;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Foundation\Auth\User as Authenticatable;
 use Illuminate\Notifications\Notifiable;
-use Laravel\Sanctum\HasApiTokens;
+use Laravel\Passport\HasApiTokens;
+use Spatie\Permission\Traits\HasRoles;
 
 class User extends Authenticatable
 {
     use HasApiTokens;
     use HasFactory;
     use Notifiable;
+    use HasRoles;
+
+    protected $guard_name = 'users';
 
     /**
      * The attributes that are mass assignable.
@@ -20,9 +24,11 @@ class User extends Authenticatable
      * @var array<int, string>
      */
     protected $fillable = [
-        'name',
+        'first_name',
+        'last_name',
         'email',
         'password',
+        'credits',
     ];
 
     /**
