bcab117342913187bf76b4745b94a9771bd3a85e
Ryan Chandra Kusuma <rookie3ryan@gmail.com>
2024-01-29 11:12:34 +0700

    fix : json response middleware
    only add application/json to header

--- app/Http/Middleware/JsonResponseMiddleware.php
+++ app/Http/Middleware/JsonResponseMiddleware.php
@@ -5,7 +5,6 @@ namespace App\Http\Middleware;
 use Closure;
 use Illuminate\Http\Request;
 use Illuminate\Http\JsonResponse;
-use Illuminate\Routing\ResponseFactory;
 use Symfony\Component\HttpFoundation\Response;
 
 class JsonResponseMiddleware
@@ -21,18 +20,6 @@ class JsonResponseMiddleware
         // dealing with a should-be JSON response.
         $request->headers->set('Accept', 'application/json');
 
-        // Get the response
-        $response = $next($request);
-
-        // If the response is not strictly a JsonResponse, we make it
-        if (!$response instanceof JsonResponse) {
-            $response = $this->responseFactory->json(
-                $response->content(),
-                $response->status(),
-                $response->headers->all()
-            );
-        }
-
-        return $response;
+        return $next($request);
     }
 }
