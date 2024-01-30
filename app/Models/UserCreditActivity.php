d595f5a61fdc718627dc5631214bcfbf0f982741
Ryan Chandra Kusuma <rookie3ryan@gmail.com>
2024-01-30 15:41:47 +0700

    models : user credit activity
    added models user credit activity for user_credit_activities table

---
+++ app/Models/UserCreditActivity.php
@@ -0,0 +1,11 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+
+class UserCreditActivity extends Model
+{
+    use HasFactory;
+}
